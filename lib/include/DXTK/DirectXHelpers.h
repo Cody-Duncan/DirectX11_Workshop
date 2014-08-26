8d6fe39698563de7b1b5cc4128659716061dad46
Cody Duncan <CodyDuncan1260@live.com>
2014-08-25 23:38:33 -0700

    Tabified DirectXHelpers and added more SetDebugObjectName overloads.

--- lib/include/DXTK/DirectXHelpers.h
+++ lib/include/DXTK/DirectXHelpers.h
@@ -55,66 +55,102 @@
 
 namespace DirectX
 {
-    // simliar to std::lock_guard for exception-safe Direct3D 11 resource locking
-    class MapGuard : public D3D11_MAPPED_SUBRESOURCE
-    {
-    public:
-        MapGuard( _In_ ID3D11DeviceContext* context,
-                  _In_ ID3D11Resource *resource,
-                  _In_ UINT subresource,
-                  _In_ D3D11_MAP mapType,
-                  _In_ UINT mapFlags )
-            : mContext(context), mResource(resource), mSubresource(subresource)
-        {
-            HRESULT hr = mContext->Map( resource, subresource, mapType, mapFlags, this );
-            if (FAILED(hr))
-            {
-                throw std::exception();
-            }
-        }
+	// simliar to std::lock_guard for exception-safe Direct3D 11 resource locking
+	class MapGuard : public D3D11_MAPPED_SUBRESOURCE
+	{
+	public:
+		MapGuard( _In_ ID3D11DeviceContext* context,
+				  _In_ ID3D11Resource *resource,
+				  _In_ UINT subresource,
+				  _In_ D3D11_MAP mapType,
+				  _In_ UINT mapFlags )
+			: mContext(context), mResource(resource), mSubresource(subresource)
+		{
+			HRESULT hr = mContext->Map( resource, subresource, mapType, mapFlags, this );
+			if (FAILED(hr))
+			{
+				throw std::exception();
+			}
+		}
 
-        ~MapGuard()
-        {
-            mContext->Unmap( mResource, mSubresource );
-        }
+		~MapGuard()
+		{
+			mContext->Unmap( mResource, mSubresource );
+		}
 
-        uint8_t* get() const
-        {
-            return reinterpret_cast<uint8_t*>( pData );
-        }
-        uint8_t* get(size_t slice) const
-        {
-            return reinterpret_cast<uint8_t*>( pData ) + ( slice * DepthPitch );
-        }
+		uint8_t* get() const
+		{
+			return reinterpret_cast<uint8_t*>( pData );
+		}
+		uint8_t* get(size_t slice) const
+		{
+			return reinterpret_cast<uint8_t*>( pData ) + ( slice * DepthPitch );
+		}
 
-        uint8_t* scanline(size_t row) const
-        {
-            return reinterpret_cast<uint8_t*>( pData ) + ( row * RowPitch );
-        }
-        uint8_t* scanline(size_t slice, size_t row) const
-        {
-            return reinterpret_cast<uint8_t*>( pData ) + ( slice * DepthPitch ) + ( row * RowPitch );
-        }
+		uint8_t* scanline(size_t row) const
+		{
+			return reinterpret_cast<uint8_t*>( pData ) + ( row * RowPitch );
+		}
+		uint8_t* scanline(size_t slice, size_t row) const
+		{
+			return reinterpret_cast<uint8_t*>( pData ) + ( slice * DepthPitch ) + ( row * RowPitch );
+		}
 
-    private:
-        ID3D11DeviceContext*    mContext;
-        ID3D11Resource*         mResource;
-        UINT                    mSubresource;
+	private:
+		ID3D11DeviceContext*    mContext;
+		ID3D11Resource*         mResource;
+		UINT                    mSubresource;
 
-        MapGuard(MapGuard const&);
-        MapGuard& operator= (MapGuard const&);
-    };
+		MapGuard(MapGuard const&);
+		MapGuard& operator= (MapGuard const&);
+	};
 
 
-    // Helper sets a D3D resource name string (used by PIX and debug layer leak reporting).
-    template<UINT TNameLength>
-    inline void SetDebugObjectName(_In_ ID3D11DeviceChild* resource, _In_z_ const char (&name)[TNameLength])
-    {
-        #if !defined(NO_D3D11_DEBUG_NAME) && ( defined(_DEBUG) || defined(PROFILE) )
-            resource->SetPrivateData(WKPDID_D3DDebugObjectName, TNameLength - 1, name);
-        #else
-            UNREFERENCED_PARAMETER(resource);
-            UNREFERENCED_PARAMETER(name);
-        #endif
-    }
+	// Helper sets a D3D resource name string (used by PIX and debug layer leak reporting).
+	template<UINT TNameLength>
+	inline void SetDebugObjectName(_In_ ID3D11DeviceChild* resource, _In_z_ const char (&name)[TNameLength])
+	{
+		#if !defined(NO_D3D11_DEBUG_NAME) && ( defined(_DEBUG) || defined(PROFILE) )
+			resource->SetPrivateData(WKPDID_D3DDebugObjectName, TNameLength - 1, name);
+		#else
+			UNREFERENCED_PARAMETER(resource);
+			UNREFERENCED_PARAMETER(name);
+		#endif
+	}
+
+	// Helper sets a D3D resource name string (used by PIX and debug layer leak reporting).
+	template<UINT TNameLength>
+	inline void SetDebugObjectName(_In_ IDXGIObject* resource, _In_z_ const char (&name)[TNameLength])
+	{
+		#if !defined(NO_D3D11_DEBUG_NAME) && ( defined(_DEBUG) || defined(PROFILE) )
+			resource->SetPrivateData(WKPDID_D3DDebugObjectName, TNameLength - 1, name);
+		#else
+			UNREFERENCED_PARAMETER(resource);
+			UNREFERENCED_PARAMETER(name);
+		#endif
+	}
+
+	// Helper sets a D3D resource name string (used by PIX and debug layer leak reporting).
+	template<UINT TNameLength>
+	inline void SetDebugObjectName(_In_ ID3D11Device* resource, _In_z_ const char (&name)[TNameLength])
+	{
+		#if !defined(NO_D3D11_DEBUG_NAME) && ( defined(_DEBUG) || defined(PROFILE) )
+			resource->SetPrivateData(WKPDID_D3DDebugObjectName, TNameLength - 1, name);
+		#else
+			UNREFERENCED_PARAMETER(resource);
+			UNREFERENCED_PARAMETER(name);
+		#endif
+	}
+
+	inline void ReportLiveObjects(ID3D11Device* device)
+	{
+		if(device)
+		{
+			ID3D11Debug* debugDev;
+			device->QueryInterface(__uuidof(ID3D11Debug), reinterpret_cast<void**>(&debugDev));
+			debugDev->ReportLiveDeviceObjects(D3D11_RLDO_DETAIL);
+		}
+	}
+
+	
 }
