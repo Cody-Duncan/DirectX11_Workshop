2a344c05bd70f5ebdbb24587cb73c76ef77943f5
Cody Duncan <cody.duncan@digipen.edu>
2014-09-18 23:25:22 -0700

    Refactored to remove include folder. Doesn't play nice with Visual Studio. Added standard stl libs to precompiled header.

--- source/DirectX11_Workshop/pch.h
+++ source/DirectX11_Workshop/pch.h
@@ -1,5 +1,24 @@
 #pragma once
 
+// STL headers
+#include <memory>
+#include <limits>
+#include <string>
+
+#include <array> 
+#include <vector>
+#include <list>
+#include <unordered_map>
+#include <stack> 	
+#include <queue>
+
+#include <algorithm>
+#include <functional>
+
+#include <cmath>
+#include <random>
+#include <numeric>
+
 //DirectX Tookit
 #include "DXTK/Audio.h"
 #include "DXTK/CommonStates.h"
